7a4b9bc94021e2136fa824b87e7aff9dc67e91c3
Josef Sivčo <j.sivco22@gmail.com>
2024-12-13 19:59:29 +0100

    Web rebuild to sasss

--- www/web/js/config/page.js
+++
@@ -1,9 +0,0 @@
-export var pageList = {
-    'projecList':{
-        index:'projectlist.html',
-        onPageRedir: true, // true = animated open div // false = url redirect to page
-        php: false,
-        
-    }
-
-}
